6f67dc3722052b6557cf352515f1e06741b49431
balakrishna88 <42884357+balakrishna88@users.noreply.github.com>
2025-09-22 17:01:31 +0530

    Update settings.py

--- affiliate/settings.py
+++ affiliate/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8==av**%(2)(0+@5i70#alh-9zpa&fau7bgr6ow!c*$9vd)xip"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
